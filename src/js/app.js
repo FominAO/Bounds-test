3f1f667c69e67499ded8d4e23acfb18b3f916bb3
FominAO <fomagoafterme@yandex.ru>
2018-06-23 22:06:48 +0300

    change margin of middle wave to 250px, change cout of records spawn

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,15 @@ import Scroll from './scroll';
 import Record from './records';
 import RecordList from './record-list' 
 window.onload = cloud();
-let newRecords = new RecordList(4)
-newRecords.render()
+let width = window.innerWidth
+if (width >= 2150) { 
+  let newRecords = new RecordList(8)
+  newRecords.render()
+} else {
+  let newRecords = new RecordList(6)
+  newRecords.render()
+}
+
 const mid = new Scroll("middle", -0.8, 250);
 const back = new Scroll("back", 0.2, 2000);
 mid.scroll();
